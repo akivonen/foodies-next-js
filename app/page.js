6e6375900971df45dcf602c26c4e3da725a1521d
Artjom Kivonen <kivonen@gmail.com>
2025-01-03 15:41:51 +0200

    add pages meals, community

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
+import Link from 'next/link';
+
+import classes from './page.module.css';
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <p>main</p>
+      </main>
+    </>
   );
 }
-
